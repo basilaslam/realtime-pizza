afa3c27cef0185deef1c11223b0139cf2225ad5e
Basil Aslam <basilaslam@pop-os.localdomain>
2020-09-03 13:19:30 +0530

    add cart page and auth

--- server.js
+++ server.js
@@ -7,12 +7,6 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3300
 
-//Assets
-app.use(express.static('public'))
-
-app.get('/', (req,res) => {
-    res.render('home.ejs')
-})
 
 
 //
@@ -20,6 +14,26 @@ app.get('/', (req,res) => {
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
+
+//Assets
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.render('home.ejs')
+})
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+})
+
+
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
+
 
 
 app.listen(PORT,() => {
