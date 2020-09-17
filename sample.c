32f639850c442cdafd842a666c5a7e6c2db31a8f
Basil Aslam <basilaslam@pop-os.localdomain>
2020-09-17 20:43:57 +0530

    add to cart

---
+++ sample.c
@@ -0,0 +1,6 @@
+int main(int argc, char const *argv[])
+{
+    int i=10;
+    printf("hello world");
+    return 0;
+}
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const expressLayout = require('express-ejs-layouts')
@@ -5,35 +6,67 @@ const path = require('path')
 
 
 const app = express()
-const PORT = process.env.PORT || 3300
+const PORT = process.env.PORT || 3000
+const mongoose = require('mongoose')
+const session = require('express-session')
+const flash = require('express-flash')
+const { MongoStore } = require('connect-mongo')
+const MongoDbStore = require('connect-mongo')(session)
 
 
 
-//
+
+//Database connection
+const url = 'mongodb://localhost/pizza';
+
+
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true, 
+    useUnifiedTopology: true, useFindAndModify:true});
+
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected...');
+    }).catch(err => {
+        console.log('Connection failed...')
+    });
 
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+
+require('./routes/web')(app)
+
+//Session Store
+
+let mongooseStore = new MongoDbStore({
+
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+//Session config
+
+app.use(session({ 
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: MongoStore,
+    saveUninitialized: false,
+    //cookie: { maxAge: 1000 * 60 * 60 * 24 } //24 hours
+    cookie: { maxAge: 1000 * 15}
+
+
+}))
+
+app.use(flash())
+
+
+
 //Assets
 app.use(express.static('public'))
 
-app.get('/', (req,res) => {
-    res.render('home.ejs')
-})
-app.get('/cart', (req, res) => {
-    res.render('customers/cart')
-})
 
-
-app.get('/login', (req, res) => {
-    res.render('auth/login')
-})
-app.get('/register', (req, res) => {
-    res.render('auth/register')
-})
-
-
+ 
 
 
 app.listen(PORT,() => {
